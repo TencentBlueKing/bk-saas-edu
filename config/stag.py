3806a4b55976d67bd8a52fd2fb045ce403827ec4
bk-blueking <contactus_bk@tencent.com>
2022-03-18 19:28:57 +0800

    minor: 修改 config/stag.py CORS_ALLOW_CREDENTIALS变量值

--- config/stag.py
+++ config/stag.py
@@ -34,7 +34,7 @@ if FRONTEND_BACKEND_SEPARATION:
     # 该跨域中间件需要放在前面
     MIDDLEWARE = ("corsheaders.middleware.CorsMiddleware",) + MIDDLEWARE
     CORS_ORIGIN_ALLOW_ALL = True
-    CORS_ALLOW_CREDENTIALS = True
+    CORS_ALLOW_CREDENTIALS = False
 
 # DATABASES.update(
 #     {
