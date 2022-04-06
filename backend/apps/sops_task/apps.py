8424d8e094a00906f41e7f1d618567a1a33d69c0
homholueng <homholueng@gmail.com>
2022-04-06 15:50:22 +0800

    feature: add periodic task config

--- backend/apps/sops_task/apps.py
+++ backend/apps/sops_task/apps.py
@@ -1,5 +1,19 @@
+from multiprocessing.spawn import import_main_path
 from django.apps import AppConfig
+from celery.schedules import crontab
+
+from blueapps.core.celery import celery_app
 
 
 class SopsTaskConfig(AppConfig):
     name = "apps.sops_task"
+
+    def ready(self) -> None:
+        celery_app.conf.beat_schedule = {
+            # Executes at sunset in Melbourne
+            "update_sops_task_status": {
+                "task": "apps.sops_task.celery.tasks.update_sops_task_status",
+                "schedule": crontab(minute=1),
+                "args": (16, 16),
+            },
+        }
