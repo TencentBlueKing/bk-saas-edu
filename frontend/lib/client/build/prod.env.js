aaa478afa0428629cdeb568d578df3f086d27c98
xuzhan <xuzhan@tencent.com>
2022-04-08 12:00:15 +0800

    update api

--- frontend/lib/client/build/prod.env.js
+++ frontend/lib/client/build/prod.env.js
@@ -20,5 +20,5 @@ module.exports = {
     USER_INFO_URL: JSON.stringify('/user/getUser'),
     LOGIN_SERVICE_URL: JSON.stringify(process.env.BK_LOGIN_URL),
     AJAX_URL_PREFIX: JSON.stringify('/api'),
-    API_URL: JSON.stringify('http://apps.paas-edu.bktencent.com/stag--bk-saas-edu-v3/')
+    API_URL: JSON.stringify('http://apps.paas-edu.bktencent.com/stag--default--bk-saas-edu-v3/')
 }
