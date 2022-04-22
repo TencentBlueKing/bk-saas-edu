a4f8c50f2f77916fd117b50b485e539d67ee03f7
wklken <wklken@gmail.com>
2022-04-22 11:15:35 +0800

    refactor(iam): codes

--- backend/common/permission.py
+++ backend/common/permission.py
@@ -10,6 +10,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from typing import List, Dict
 
 from django.conf import settings
 
@@ -19,21 +20,23 @@ from iam import IAM, Request, Subject, Action, Resource
 from iam.apply.models import ActionWithoutResources, ActionWithResources, Application, RelatedResourceType, ResourceInstance, ResourceNode
 from common.constants import ActionEnum, ResourceTypeEnum
 
+# NOTE: here just enable iam logger, logger level is debug temporarily
+#       should change to logging.ERROR in production
 import sys
 import logging
 iam_logger = logging.getLogger("iam")
 iam_logger.setLevel(logging.DEBUG)
 
-debug_hanler = logging.StreamHandler(sys.stdout)
-debug_hanler.setFormatter(logging.Formatter('%(levelname)s [%(asctime)s] [IAM] %(message)s'))
-iam_logger.addHandler(debug_hanler)
+debug_handler = logging.StreamHandler(sys.stdout)
+debug_handler.setFormatter(logging.Formatter('%(levelname)s [%(asctime)s] [IAM] %(message)s'))
+iam_logger.addHandler(debug_handler)
 
 
 class Permission(object):
     def __init__(self):
         self._iam = IAM(settings.APP_CODE, settings.SECRET_KEY, bk_apigateway_url=settings.BK_IAM_APIGATEWAY_URL)
 
-    def _make_request_without_resources(self, username, action_id):
+    def _make_request_without_resources(self, username: str, action_id: str) -> Request:
         request = Request(
             settings.BK_IAM_SYSTEM_ID,
             Subject("user", username),
@@ -43,7 +46,7 @@ class Permission(object):
         )
         return request
 
-    def _make_request_with_resources(self, username, action_id, resources):
+    def _make_request_with_resources(self, username: str, action_id: str, resources: List[Resource]) -> Request:
         request = Request(
             settings.BK_IAM_SYSTEM_ID,
             Subject("user", username),
@@ -53,17 +56,17 @@ class Permission(object):
         )
         return request
 
-    def allowed_task_create(self, username):
+    def allowed_task_create(self, username: str) -> bool:
         request = self._make_request_without_resources(username, ActionEnum.TASK_CREATE.value)
         return self._iam.is_allowed(request)
 
-    def allowed_task_view(self, username, task_id):
+    def allowed_task_view(self, username: str, task_id: str) -> bool:
         r = Resource(settings.BK_IAM_SYSTEM_ID, ResourceTypeEnum.TASK.value, task_id, {})
         resources = [r]
         request = self._make_request_with_resources(username, ActionEnum.TASK_VIEW.value, resources)
         return self._iam.is_allowed(request)
 
-    def batch_allowed_task_view(self, username, task_ids):
+    def batch_allowed_task_view(self, username: str, task_ids: List[str]) -> Dict[str, bool]:
         resources_list = []
         for task_id in task_ids:
             r = Resource(settings.BK_IAM_SYSTEM_ID, ResourceTypeEnum.TASK.value, task_id, {})
@@ -82,13 +85,13 @@ class Permission(object):
         # call batch_is_allowed
         return self._iam.batch_is_allowed(request, resources_list)
 
-    def make_action_application(self, action_id):
+    def make_action_application(self, action_id: str) -> Application:
         action = ActionWithoutResources(action_id)
         actions = [action]
         application = Application(settings.BK_IAM_SYSTEM_ID, actions)
         return application
 
-    def make_task_application(self, task_id):
+    def make_task_application(self, task_id: str) -> Application:
         task_name = f"task:{task_id}"
         try:
             task_name = Tasks.objects.get(id=task_id).task_name
@@ -105,7 +108,7 @@ class Permission(object):
         application = Application(settings.BK_IAM_SYSTEM_ID, actions)
         return application
 
-    def generate_apply_url(self, application, bk_username=""):
+    def generate_apply_url(self, application: Application, bk_username: str = "") -> str:
         """
         处理无权限 - 跳转申请列表
         """
